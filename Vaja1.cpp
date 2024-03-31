b150bc81f5d48718f2a3b31b01b961fc5717a8f8
Kenoo330 <kenan.vrtagic@gmail.com.com>
2024-03-31 23:31:45 +0200

    Dodajanje funkcije countingSort

--- Vaja1.cpp
+++ Vaja1.cpp
@@ -20,6 +20,32 @@ bool Branje_Stevil(vector<unsigned char> &vec, const char s[]) {
     return true;
 }
 
+void countingSort(vector<unsigned char> &A, int n, int k) {
+    vector<int> count(2, 0);
+    vector<unsigned char> output(n);
+
+    // Count the occurrences of each bit
+    for (int i = 0; i < n; ++i) {
+        ++count[(A[i] >> k) & 1];
+    }
+
+    // Calculate cumulative counts
+    for (int i = 1; i < 2; ++i) {
+        count[i] += count[i - 1];
+    }
+
+    // Place the elements in sorted order
+    for (int i = n - 1; i >= 0; --i) {
+        output[count[(A[i] >> k) & 1] - 1] = A[i];
+        --count[(A[i] >> k) & 1];
+    }
+
+    // Copy sorted elements back to the original array
+    for (int i = 0; i < n; ++i) {
+        A[i] = output[i];
+    }
+}
+
 void radixSort(vector<unsigned char> &A) {
     for (int k = 0; k < 8; ++k) {
         countingSort(A, A.size(), k);
